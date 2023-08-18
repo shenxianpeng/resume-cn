e5c6b16c617e1b94dc80a27dd2f3413758b9af73
shenxianpeng <xianpeng.shen@gmail.com>
2023-08-18 12:37:30 +0800

    Update resume

--- publish.sh
+++ publish.sh
@@ -1,10 +1,7 @@
 echo "Start of publish"
-rm -rf .git
-git init
-git remote add origin git@github.com:shenxianpeng/resume-cn.git
 git add --all
 git config user.name shenxianpeng
 git config user.email "xianpeng.shen@gmail.com"
-git commit -m "Inital Commit"
-git push -f origin master
+git commit -m "Update resume"
+git push origin master
 echo "End of publish"
