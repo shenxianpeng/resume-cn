7316c19bc1ab53f558c6dcc43f6d61d9fc711899
shenxianpeng <xianpeng.shen@gmail.com>
2023-12-07 14:26:49 +0800

    Update resume

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@ git add --all
 git config user.name shenxianpeng
 git config user.email "xianpeng.shen@gmail.com"
 git commit -m "Update resume"
-git push origin master
+git push -u origin HEAD:main
 echo "End of publish"
